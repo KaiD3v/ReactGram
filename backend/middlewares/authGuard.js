d7936112980a95e31669259b47117cc877c8d949
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-05-10 22:08:18 -0300

    get auth user

--- backend/middlewares/authGuard.js
+++ backend/middlewares/authGuard.js
@@ -1,20 +1,26 @@
 const User = require("../models/User");
+
 const jwt = require("jsonwebtoken");
+
 const jwtSecret = process.env.JWT_SECRET;
 
 const authGuard = async (req, res, next) => {
-  const authHeader = req.header["authorization"];
-  const token = authHeader && authHeader.split("")[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-  // check if header has a token
-  if (!token) return res.status(402).json({ errors: ["Acesso negado"] });
+  // Check if header has a token
+  if (!token) return res.status(401).json({ errors: ["Acesso negado!"] });
 
+  // Check if token is valid
   try {
     const verified = jwt.verify(token, jwtSecret);
 
     req.user = await User.findById(verified.id).select("-password");
-  } catch (error) {
-    res.status(401).json({ errors: ["Token inválido!"] });
+
     next();
+  } catch (err) {
+    res.status(400).json({ errors: ["O Token é inválido!"] });
   }
 };
+
+module.exports = authGuard;
