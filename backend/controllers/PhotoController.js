400d26b11acd6896b2ef26cd167e5860105047fb
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-05-11 13:36:15 -0300

    get photo by id

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -81,9 +81,26 @@ const getUserPhotos = async (req, res) => {
   return res.status(200).json(photos);
 };
 
+// Get photo by id
+const getPhotoById = async (req, res) => {
+  const { id } = req.params;
+
+  const photo = await Photo.findById(new mongoose.Types.ObjectId(id));
+
+  // Check if photo exists
+  if (!photo) {
+    res.status(404).json({ Errors: ["Foto não encontrada."] });
+    return;
+  }
+
+  res.status(200).json(photo);
+};
+
 module.exports = {
   insertPhoto,
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
+  getPhotoById
 };
+
