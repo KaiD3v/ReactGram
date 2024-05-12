f146626145a9a3a18918807dc4d761b7224cf9f6
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-05-12 17:26:34 -0300

    router dom configs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+// Router
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+// pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
 
 function App() {
-
   return (
     <div className="App">
+      <Router>
       <h1>ReactGram</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
