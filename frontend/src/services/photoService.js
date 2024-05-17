9fa9dc096d820db560f5e1c489a4b2feff8eb7cc
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-05-17 19:40:32 -0300

    get photo service

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -44,6 +44,20 @@ const updatePhoto = async (data, id, token) => {
   }
 };
 
+// Get a photo by id
+const getPhoto = async (id) => {
+  const config = requestConfig("GET");
+  try {
+    const res = await fetch(api + "/photos/" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 // Delete a photo
 const deletePhoto = async (id, token) => {
   const config = requestConfig("DELETE", "", token);
@@ -59,5 +73,5 @@ const deletePhoto = async (id, token) => {
   }
 };
 
-const photoService = { publishPhoto, getUserPhotos, deletePhoto, updatePhoto };
+const photoService = { publishPhoto, getUserPhotos, deletePhoto, updatePhoto, getPhoto };
 export default photoService;
