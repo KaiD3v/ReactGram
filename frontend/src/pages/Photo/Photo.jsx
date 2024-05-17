e17687624ee44afa3c234499759ef9b4b753ceaa
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-05-17 19:35:39 -0300

    single photo page update

---
+++ frontend/src/pages/Photo/Photo.jsx
@@ -0,0 +1,25 @@
+import "./Photo.css";
+
+import { uploads } from "../../utils/config";
+
+// components
+import Message from "../../components/Message";
+import { Link } from "react-router-dom";
+
+// hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+
+// redux
+
+
+const Photo = () => {
+  return (
+    <div>
+      <h1>Ai caralho</h1>
+    </div>
+  );
+};
+
+export default Photo;
