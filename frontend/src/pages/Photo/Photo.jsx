245e19a59386c863012346dc5e5d128198692221
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-05-17 20:56:39 -0300

    message in photos feature

--- frontend/src/pages/Photo/Photo.jsx
+++ frontend/src/pages/Photo/Photo.jsx
@@ -11,6 +11,7 @@ import { getPhoto, like } from "../../slices/photoSlice";
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 // redux
 
@@ -22,12 +23,12 @@ const Photo = () => {
 
   const dispatch = useDispatch();
 
+  const resetMessage = useResetComponentMessage();
+
   const { user } = useSelector((state) => state.auth);
   const { photo, loading, error, message } = useSelector(
     (state) => state.photo
   );
-
-  // comments
 
   // load photo data
   useEffect(() => {
@@ -35,8 +36,9 @@ const Photo = () => {
   }, [dispatch, id]);
 
   const handleLike = () => {
-    dispatch(like(photo._id))
+    dispatch(like(photo._id));
 
+    resetMessage();
   };
 
   if (loading) {
@@ -47,6 +49,10 @@ const Photo = () => {
     <div id="photo">
       <PhotoItem photo={photo} />
       <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+      <div className="message-container">
+        {error && <Message msg={error} type="error" />}
+        {message && <Message msg={message} type="success" />}
+      </div>
     </div>
   );
 };
