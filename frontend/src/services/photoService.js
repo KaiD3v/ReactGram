7e4aca2d634c80a62b13cef6af44a163c0932e2e
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-05-17 20:11:09 -0300

    like photo  slice

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -73,11 +73,27 @@ const deletePhoto = async (id, token) => {
   }
 };
 
+// Like a Photo
+const like = async (id, token) => {
+  const config = requestConfig("PUT", null, token);
+
+  try {
+    const res = await fetch(api + "/photos/like" + id, config)
+      .then((res) => res.json())
+      .catch((err) => err);
+
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const photoService = {
   publishPhoto,
   getUserPhotos,
   deletePhoto,
   updatePhoto,
   getPhoto,
+  like,
 };
 export default photoService;
