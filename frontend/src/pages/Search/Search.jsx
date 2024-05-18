715478b11f121f366b14703e1603cb42d83a338f
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-05-17 22:21:41 -0300

    search form

--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -4,6 +4,7 @@ import "./Search.css";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
+import { useQuery } from "../../hooks/useQuery";
 
 // componetns
 import LikeContainer from "../../components/LikeContainer";
