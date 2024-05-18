7fc2bbcffd8ca4dd9077f1ee144575bf40c50a14
Kaique Ricardo de Melo <kaikricardo99@gmail.com>
2024-05-18 20:24:46 -0300

    tab bar funcionatilities

--- frontend/src/components/MobileTabBar/MobileTabBar.jsx
+++ frontend/src/components/MobileTabBar/MobileTabBar.jsx
@@ -1,7 +1,6 @@
 import "../../App.css";
 
-import { NavLink } from "react-router-dom";
-
+// Components
 import {
   BsSearch,
   BsHouseDoorFill,
@@ -12,27 +11,66 @@ import {
 
 import { IoIosExit } from "react-icons/io";
 
+import { NavLink } from "react-router-dom";
+
+// Hooks
+import { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+// Redux
+import { logout, reset } from "../../slices/authSlice";
+
 const MobileTabBar = () => {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
+
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+
+    navigate("/login");
+  };
+
   return (
     <footer id="tab-bar">
-      <ul>
-        <NavLink to="/">
-          <BsHouseDoorFill size={"1.375rem"} />
-        </NavLink>
-        <li>
-          <NavLink to={`/users/`}>
-            <BsFillCameraFill size={"1.375rem"} />
+      {auth ? (
+        <ul>
+          <NavLink to="/">
+            <BsHouseDoorFill size={"1.375rem"} />
           </NavLink>
-        </li>
-        <li>
-          <NavLink to="/profile">
-            <BsFillPersonFill size={"1.375rem"} />
-          </NavLink>
-        </li>
-        <li>
-          <IoIosExit onClick={'handleLogout'} size={"1.375rem"} />
-        </li>
-      </ul>
+          <li>
+            <NavLink to={`/users/${user._id}`}>
+              <BsFillCameraFill size={"1.375rem"} />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to={`/users/${user._id}`}>
+              <BsFillPersonFill size={"1.375rem"} />
+            </NavLink>
+          </li>
+          {user && (
+            <li>
+              <IoIosExit onClick={handleLogout} size={"1.375rem"} />
+            </li>
+          )}
+        </ul>
+      ) : (
+        <>
+          {" "}
+          <li>
+            <NavLink to="/login">Entrar</NavLink>
+          </li>
+          <li>
+            <NavLink to="/register">Cadastrar</NavLink>
+          </li>
+        </>
+      )}
     </footer>
   );
 };
